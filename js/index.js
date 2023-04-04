00832dcaf68efc4b0ad1b87954eb6d22145ec407
swj4664 <108079743+swj4664@users.noreply.github.com>
2023-04-04 18:40:10 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,34 +1,6 @@
+    let earthWrap = document.querySelector('.earthWrap')
 
-
-
-window.addEventListener("scroll", (event) => {
-    let scrollY = this.scrollY;
-    let scrollFirst = this.scrollY * -0.001/2;
-    let scrollSecond = this.scrollY * -0.001/2;
-    let earth = document.querySelector('.earth')
-    let scrollSign = document.querySelector('.scrollSign')
-    let textWrap = document.querySelector('.textWrap')
- 
-    // console.log(scrollFirst);
-    console.log(scrollY);
-    console.log(scrollSecond);
-    
-    earth.style.scale = `${1 + scrollFirst}`
-    earth.style.opacity = `${1 + scrollFirst}`
-    scrollSign.style.opacity = `${1 + scrollFirst}`
-    
-    if(scrollFirst <= -1) {
-        earth.style.scale = '0'
-    }
-
-    if(scrollY > 3000){
-        textWrap.style.display = 'block'
-    } else {
-        textWrap.style.display = 'none'
-    }
-
-    textWrap.style.scale = '1'
-    if(scrollY > 3500){
-        textWrap.style.scale = `${1 + scrollSecond}`
-    } 
-});
+    earthWrap.addEventListener('scroll', (event) => {
+        console.log(this.scrollTop)
+       
+    });
